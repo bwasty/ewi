4ca2a020cd3cd7f2518a607e1d214489f7fefc29
Benjamin Wasty <benjamin.wasty@softwarediagnostics.com>
2016-11-02 12:20:18 +0100

    add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
+import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap-theme.css';
 import './index.css';
 
 ReactDOM.render(
